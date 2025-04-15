6714ecd59bcb6a0749fcfd8efe5978b712e49fea
roman <s-3m@yandex.ru>
2025-04-15 15:52:00 +0300

    rename attribute to db class

--- src/config/db_config.py
+++ src/config/db_config.py
@@ -7,14 +7,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 class DbSettings(BaseSettings):
     model_config = SettingsConfigDict(env_file=BASE_DIR / ".env")
 
-    postgres_user: str = "postgres_user"
-    postgres_password: str = "postgres_password"
-    POSTGRES_HOST: str = "localhost"
-    postgres_port: str = "postgres_port"
-    postgres_db: str = "postgres_db"
-    db_echo: bool = True
-
+    user: str = "postgres_user"
+    password: str = "postgres_password"
+    host: str = "localhost"
+    port: str = "postgres_port"
+    name: str = "postgres_db"
+    echo: bool = True
 
     @property
-    def db_url(self) -> str:
-        return f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}@{self.POSTGRES_HOST}:{self.postgres_port}/{self.postgres_db}"
+    def url(self) -> str:
+        return f"postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{self.port}/{self.name}"
